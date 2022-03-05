dd169402eefa0eef477281068bbd3ee3eb3bc4af
SrNightmare09 <srnightmare09@gmail.com>
2022-03-06 03:40:49 +0530

    add candle light chart for price

--- src/index.py
+++ src/index.py
@@ -5,3 +5,12 @@ import plotly.graph_objs as go
 
 data = yf.download(tickers='BTC-USD', period='3mo', interval='1d')
 print(data)
+
+data['MA5'] = data['Close'].rolling(5).mean()
+data['MA20'] = data['Close'].rolling(20).mean()
+
+fig = go.Figure()
+
+fig.add_trace(go.Candlestick(x=data.index, open=data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name = 'market data'))
+
+fig.show()
