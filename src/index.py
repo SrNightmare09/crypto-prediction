7b0c02e7c2f2e42e05db640244d733e996f93813
SrNightmare09 <srnightmare09@gmail.com>
2022-03-06 03:41:09 +0530

    add death line and gold line

--- src/index.py
+++ src/index.py
@@ -13,4 +13,7 @@ fig = go.Figure()
 
 fig.add_trace(go.Candlestick(x=data.index, open=data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name = 'market data'))
 
+fig.add_trace(go.Scatter(x=data.index, y=data['MA20'], line=dict(color='black', width = 1.5), name='Death Line'))
+fig.add_trace(go.Scatter(x=data.index, y=data['MA5'], line=dict(color='orange', width=1.5), name='Gold Line'))
+
 fig.show()
