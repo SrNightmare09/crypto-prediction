621020a637858969a2268c1a68389a70e770a3d8
thefallentoaster <61287843+thefallentoaster@users.noreply.github.com>
2023-06-29 13:53:19 +0200

    Update index.py
    New features:
    -buy and sell mark
    -auto refresh

--- src/index.py
+++ src/index.py
@@ -1,5 +1,46 @@
+
+# Import of the modules
+
 import yfinance as yf
 import plotly.graph_objs as go
+import plotly.io as pio
+import time
+from selenium import webdriver
+import os
+from random import randint
+from asciimatics.screen import Screen
+from asciimatics.effects import Cycle, Stars
+from asciimatics.renderers import FigletText
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+
+# Startup Animation
+
+def demo(screen):
+    effects = [
+        Cycle(
+            screen,
+            FigletText("Gate to", font='small'),
+            int(screen.height / 2 - 8)),
+        Cycle(
+            screen,
+            FigletText("HEAVEN", font='big'),
+            int(screen.height / 2 + 3)),
+        Stars(screen, 200)
+    ]
+    screen.play([Scene(effects, 500)])
+    ev = screen.get_key()
+    if ev in (ord('Q'), ord('q')):
+         return
+    screen.refresh()
+Screen.wrapper(demo)
+
+# Main code of the Bot
+
+x = 0
+
+clear = lambda: os.system('cls')
+clear()
 
 user_crypto_currency = input('Enter the currency code of the cryptocurrency: ')
 user_currency = input('Enter the ISO code of your currency: ')
@@ -14,17 +55,63 @@ for i in range(0, len(valid_intervals)):
 
 user_interval = int(input('Enter corresponding number to the wanted interval: '))
 
-data = yf.download(tickers=user_tickers, period=user_period, interval=valid_intervals[user_interval - 1])
-print(data)
+data_refresh = int(input('In what time intervall should the bot refresh the data (seconds): '))
 
-data['MA5'] = data['Close'].rolling(5).mean()
-data['MA20'] = data['Close'].rolling(20).mean()
+# Chromedriver creation
 
-fig = go.Figure()
+driver = webdriver.Chrome()
 
-fig.add_trace(go.Candlestick(x=data.index, open=data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name = 'Market Data'))
+# Loop of recreating and refreshing the data
 
-fig.add_trace(go.Scatter(x=data.index, y=data['MA20'], line=dict(color='black', width = 1.5), name='Death Line'))
-fig.add_trace(go.Scatter(x=data.index, y=data['MA5'], line=dict(color='orange', width=1.5), name='Gold Line'))
+while True:
 
-fig.show()
+    # Data download from yahoo
+
+    data = yf.download(tickers=user_tickers, period=user_period, interval=valid_intervals[user_interval - 1])
+    print(data)
+
+    # Creating the chart
+   
+    data['MA5'] = data['Close'].rolling(5).mean()
+    data['MA20'] = data['Close'].rolling(20).mean()
+
+    fig = go.Figure()
+    fig.add_trace(go.Candlestick(x=data.index, open=data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name='Market Data'))
+    fig.add_trace(go.Scatter(x=data.index, y=data['MA20'], line=dict(color='black', width=1.5), name='Death Line'))
+    fig.add_trace(go.Scatter(x=data.index, y=data['MA5'], line=dict(color='orange', width=1.5), name='Gold Line'))
+
+    cross_points_green = []
+    for i in range(1, len(data)):
+        if (data['MA5'][i-1] < data['MA20'][i-1]) and (data['MA5'][i] >= data['MA20'][i]):
+                cross_points_green.append(data.index[i])
+
+    cross_points_red = []
+    for i in range(1, len(data)):
+        if (data['MA5'][i-1] > data['MA20'][i-1]) and (data['MA5'][i] <= data['MA20'][i]):
+                cross_points_red.append(data.index[i])
+   
+    # Ad the markers to the chart
+    fig.add_trace(go.Scatter(x=cross_points_green, y=data.loc[cross_points_green, 'Close'], mode='markers', marker=dict(color='green'), name='Buy'))
+    fig.add_trace(go.Scatter(x=cross_points_red, y=data.loc[cross_points_red, 'Close'], mode='markers', marker=dict(color='red'), name='Sell'))
+   
+
+    # Saving the chart as html
+
+    plot_file = f"plot.html"
+    fig.write_html(plot_file)
+
+    # Opening the chart
+
+    if x == 0:
+        driver.get(f"file:///C:/Users/valen/Desktop/crypto-prediction-master/{plot_file}")
+        x = x +1
+    else:
+        driver.execute_script(f"window.location.href = 'file:///C:/Users/valen/Desktop/crypto-prediction-master/{plot_file}';")
+
+    time.sleep(data_refresh)
+
+    # Deleting the html file
+
+    os.remove(plot_file)
+
+driver.quit()
